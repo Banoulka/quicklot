b7547119f3b2bfc49074ecb11fb4e0d87f76b09d
Nathan Bamford <nathanbammy@hotmail.co.uk>
2020-12-18 16:26:10 +0000

    uhh public change

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 // Bootstrap for the project
 use app\bframe\facades\Route;
 
-require_once __DIR__ . "/../app/bframe/bootstrap.php";
+require_once __DIR__ . "/app/bframe/bootstrap.php";
 
 // Routes for the project
-require_once  __DIR__ . "/../routes.php";
+require_once  __DIR__ . "/routes.php";
 
 // Some config items
-require_once __DIR__ . "/../config/config.php";
+require_once __DIR__ . "/config/config.php";
 
 // Resolve the routes in index
 Route::resolve();
